b8f42e4ae4eb5e954b6b5e7f4b8515744612356c
Luis Tierrafria <luis.enri1392@gmail.com>
2025-05-19 14:40:56 -0600

    fix error in localstorage

--- src/utils/IsLogin.ts
+++ src/utils/IsLogin.ts
@@ -1,4 +1,8 @@
 export default function isLogIn() {
+  if (typeof window === 'undefined') {
+    return false
+  }
+
   const token = localStorage?.getItem('token')
   if (token) {
     return true
--- src/utils/isAdmin.ts
+++ src/utils/isAdmin.ts
@@ -1,4 +1,7 @@
 export default function isAdmin() {
+  if (typeof window === 'undefined') {
+    return false
+  }
   const roles = localStorage?.getItem('roles')
   if (roles === 'admin') {
     return true
